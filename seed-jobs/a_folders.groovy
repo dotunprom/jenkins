6d5b7c06d3181e5e327b2ba3f29b94028a94b85f
dotunakin <dotunprom@gmail.com>
2022-09-29 12:49:23 +0100

    Parameter

--- seed-jobs/a_folders.groovy
+++ seed-jobs/a_folders.groovy
@@ -2,7 +2,3 @@ folder('CI-Pipelines') {
     displayName('CI-Pipelines')
     description('CI-Pipelines')
 }
-
-folder('project-a/testing') {
-    description('Folder containing all QA jobs for project A')
-}
